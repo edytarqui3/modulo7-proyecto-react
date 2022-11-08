9d34f0ac05e0d8a6cae4d7cccd7ec535aee89ee0
Edy Tarqui <edytarqui@gmail.com>
2022-11-08 00:16:50 -0400

    Updates

---
+++ storybook/react-syntax-highlighter_languages_highlight_ebnf.f6b18a3fdbf3ab6f6d70.bundle.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[49],{1531:function(module,exports){module.exports=function(hljs){var commentMode=hljs.COMMENT(/\(\*/,/\*\)/);return{illegal:/\S/,contains:[commentMode,{className:"attribute",begin:/^[ ]*[a-zA-Z][a-zA-Z-_]*([\s-_]+[a-zA-Z][a-zA-Z]*)*/},{begin:/=/,end:/[.;]/,contains:[commentMode,{className:"meta",begin:/\?.*\?/},{className:"string",variants:[hljs.APOS_STRING_MODE,hljs.QUOTE_STRING_MODE,{begin:"`",end:"`"}]}]}]}}}}]);
+//# sourceMappingURL=react-syntax-highlighter_languages_highlight_ebnf.f6b18a3fdbf3ab6f6d70.bundle.js.map
