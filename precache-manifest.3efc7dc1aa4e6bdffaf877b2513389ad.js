457d7165c343d4f13ce6de25d9bea2b45c3ce511
Edy Tarqui <edytarqui@gmail.com>
2022-11-08 00:07:14 -0400

    Updates

---
+++ precache-manifest.3efc7dc1aa4e6bdffaf877b2513389ad.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "378ecb8d1649de158c00eaa9e87c1919",
+    "url": "/modulo7-proyecto-react/index.html"
+  },
+  {
+    "revision": "f9134f34d0b2dc29a75f",
+    "url": "/modulo7-proyecto-react/static/css/main.c8882741.chunk.css"
+  },
+  {
+    "revision": "b31d0b0678c3f0283106",
+    "url": "/modulo7-proyecto-react/static/js/2.482abe33.chunk.js"
+  },
+  {
+    "revision": "938add99345225e008d704b39b4ce7cb",
+    "url": "/modulo7-proyecto-react/static/js/2.482abe33.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9134f34d0b2dc29a75f",
+    "url": "/modulo7-proyecto-react/static/js/main.6c16977b.chunk.js"
+  },
+  {
+    "revision": "18bc7fa93169641aed9a",
+    "url": "/modulo7-proyecto-react/static/js/runtime-main.9f4c593c.js"
+  },
+  {
+    "revision": "6ebab8071138ef2da892ef17e9330391",
+    "url": "/modulo7-proyecto-react/static/media/logo.6ebab807.png"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/modulo7-proyecto-react/precache-manifest.3efc7dc1aa4e6bdffaf877b2513389ad.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/modulo7-proyecto-react/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
